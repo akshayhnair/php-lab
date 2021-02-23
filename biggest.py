2cafe0cf0ee0977490a7816686af98e3af9b186c
akshayhnair <75111906+akshayhnair@users.noreply.github.com>
2021-02-23 23:02:48 +0530

    Add files via upload

---
+++ biggest.py
@@ -0,0 +1,9 @@
+a = int (input("enter the number1: "))
+b = int (input("enter the number2: "))
+c = int (input("enter the number32:"))
+if a > b and a > c:
+    print("a is the greater",a)
+elif b > c:
+        print("b is greater",b)
+else:
+    print("c is greater",c)
